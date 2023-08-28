fcd81b8273055d1ae55202c54b08ba2516575366
Dejan <dejan.ljubej@gmail.com>
2023-08-28 19:48:11 +0200

    made the projects pannel dynamic, next integrate firebase

--- src/app/components/games-page/games-page.component.ts
+++ src/app/components/games-page/games-page.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesPageComponent implements OnInit {
 
+  items: object[] = [,,,,];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }

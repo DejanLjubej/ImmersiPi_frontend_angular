d4666aa4cb3b53430defd39c776862880a14f596
Dejan <dejan.ljubej@gmail.com>
2023-08-29 19:02:17 +0200

    Integrated Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { RoutingModule } from './routing/routing.module';
 
+import { environment } from '../environments/environment';
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FrontPageComponent } from './components/front-page/front-page.component';
@@ -15,6 +18,17 @@ import { FooterUpperComponent } from './components/footer/footer-upper/footer-up
 import { FeaturedGameComponent } from './components/front-page/featured-game/featured-game.component';
 import { GamesPageComponent } from './components/games-page/games-page.component';
 import { PageUnderConstructionComponent } from './components/page-under-construction/page-under-construction.component';
+
+// Firebase
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [
@@ -34,8 +48,18 @@ import { PageUnderConstructionComponent } from './components/page-under-construc
     HttpClientModule,
     NgbModule,
     FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'immersipilandingdata',
+    appId: '1:983584330728:web:ad58a5b4906eb3c8810a5d',
+    databaseURL: 'https://immersipilandingdata-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'immersipilandingdata.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDXBoM1EcF1XZwXe_ZT0uwUcuNuJxUhZNA',
+    authDomain: 'immersipilandingdata.firebaseapp.com',
+    messagingSenderId: '983584330728',
+    measurementId: 'G-LYRHL3CYW9',
+  },
   production: true
 };
